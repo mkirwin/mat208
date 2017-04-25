207a9ce46988c6cd54ea9748aed42458f1434373
mkirwin <kirwinma@grinnell.edu>
2017-04-24 21:23:22 -0500

    homework 14 started

---
+++ hw/hw14.ml
@@ -0,0 +1,6 @@
+datatype suit = Club | Diamond | Heart | Spade;
+datatype rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King;
+type card = rank*suit;
+
+val allSuits = [Club,Diamond,Spade,Heart];
+val allRanks = [Ace,Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King];
